07249b4e08d40624d219cb39cb68c0343a68a2e0
angyalzsolt <angyal.zsolt@hotmail.hu>
2019-03-07 16:02:39 +0100

    check for input

--- indexx.js
+++ indexx.js
@@ -4,13 +4,19 @@
 let board;
 let ship1_row;
 let ship1_col;
+
+const guessRowEl = document.getElementById('guessRow');
+const guessColEl = document.getElementById('guessCol');
 // let board;
 function startGame(size){
+	if(isNaN(size) || size > 6){
+		return sendMsg('Maximum battlfield size is 6.');
+	} 
 	board = getBoard(size);
-	console.log('Board', board.length);
+	// console.log('Board', board.length);
 	ship1_row = randomPos(board);
 	ship1_col = randomPos(board);
-	console.log('Ship', ship1_row, ship1_col)
+	// console.log('Ship', ship1_row, ship1_col)
 	showBoard(board);	
 }	
 
@@ -32,34 +38,46 @@ function randomPos(board){
 
 
 const makeGuess = (row, col)=>{
+	if(row === '' || col === ''){
+		return sendMsg('Give both parameter');
+	}
 	let guess_row = parseInt(row) - 1;
 	let guess_col = parseInt(col) - 1;
-	console.log('Board length: ', board.length);
-	console.log('Row', guess_row, 'Col', guess_col);
-	if(guess_row == ship1_row && guess_col == ship1_col){
-		sendMsg('Congratulations, you destroyed the SHIP ONE');
-		console.log('Congratulations, you destroyed the SHIP ONE');
-		alert('You won');
+	// console.log('Board length: ', board.length);
+	// console.log('Row', guess_row, 'Col', guess_col);
+
+	try{
+		if(guess_row == ship1_row && guess_col == ship1_col){
+		sendMsg('Congratulations, you destroyed the ship');
+		alert(`Nice job, the ship was on row: ${row}, and column: ${col}`);
 		resetBoard();
 	} else {
 		if(guess_row >= board.length || guess_col >= board.length){
 			sendMsg('That\s not even in the ocean.');
-			console.log('That\s not even in the ocean, cuz board is: ', board.length);
 			return
 		} else if(board[guess_row][guess_col] == 'X'){
 			sendMsg('You have already guessed it.');
-			console.log('You have already guessed it.');
 		} else {
 			sendMsg('You missed it, try again');
-			console.log('You missed it, try again.')
 			board[guess_row][guess_col] = 'X';
+			guessRowEl.value = '';
+			guessColEl.value = '';
+			guessRowEl.focus();
 		}
-	showBoard(board);	
+		showBoard(board);
 	}
+} catch(e){
+	sendMsg('Invalid input');
+}
+
+	
+
 }
 
 function resetBoard(){
 	document.querySelector('#target').innerHTML = '<h2>Battlefield</h2>';
+	guessRowEl.value = '';
+	guessColEl.value = '';
 	board = [];
 	ship1_row = 0;
 	ship1_col = 0;
@@ -75,7 +93,8 @@ document.querySelector('#start').addEventListener('submit', (e)=>{
 	e.preventDefault();
 	let s = e.target.elements.boardSize.value;
 	let f = parseInt(s)
-	console.log(s)
+	console.log('the type is ', typeof f, f)
+
 	startGame(f);
 })
 
@@ -86,6 +105,8 @@ document.querySelector('#guess').addEventListener('submit', (e)=>{
 	row = e.target.elements.guessRow.value;
 	col = e.target.elements.guessCol.value;
 	makeGuess(row, col);
+	// document.getElementById('guessRow').value = '';
+	// document.getElementById('guessCol').value = '';
 })
 
 
